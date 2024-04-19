bbfc84dcbea5fb98b5b4918e1d6d7d9ae55d50bd
CrLeCook <112937952+CrLeCook@users.noreply.github.com>
2024-04-19 14:26:04 +0100

    styling changes

--- application/forms.py
+++ application/forms.py
@@ -6,5 +6,5 @@ from wtforms.validators import DataRequired
 class TodoForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     description = TextAreaField('description',validators=[DataRequired()])
-    completed = SelectField('Completed', choices = [("False", "False"), ("True", "True")], validators = [DataRequired()])
-    submit = SubmitField("Add todo")
+    completed = SelectField('completed?', choices = [("False", "False"), ("True", "True")], validators = [DataRequired()])
+    submit = SubmitField("create task!")
