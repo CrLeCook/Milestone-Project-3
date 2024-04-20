b68e6d7d3cc3582c0b078d5bb7a46eb73e289952
CrLeCook <112937952+CrLeCook@users.noreply.github.com>
2024-04-20 16:27:33 +0100

    deleted/updated todo routes added

--- application/routes.py
+++ application/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect, flash, request, url_for
 from application import db
 from .forms import TodoForm
 from datetime import datetime
+from bson import ObjectId
 
 @app.route("/")
 def index():
@@ -10,12 +11,14 @@ def index():
 
 @app.route("/tech")
 def tech():
-    return render_template("tech.html", title = "To Do | Habitual")
+    return render_template("tech.html")
+
+@app.route("/view_todos")
 def get_todos():
     todos = []
-    for todo in db.todos_flask.find().sort("date_created", -1):
+    for todo in db.todos_flask.find().sort("date completed", -1):
         todo["_id"] = str(todo["_id"])
-        todo["date_created"] = todo["date_created"].strftime("%b %d %Y %H:%M:%S")
+        todo["date completed"] = todo["date completed"].strftime("%b %d %Y %H:%M:%S")
         todos.append(todo)
 
     return render_template("view_todos.html", todos = todos)
@@ -35,7 +38,40 @@ def add_todo():
             "date completed": datetime.now()
         })
         flash("Task successfully created", "success")
-        return redirect("/")
+        return redirect("/view_todos")
     else:
         form = TodoForm()
+    return render_template("add_todo.html", form = form)
+
+@app.route("/delete_todo/<id>")
+def delete_todo(id):
+    db.todos_flask.find_one_and_delete({"_id": ObjectId(id)})
+    flash("Task successfully deleted", "success")
+    return redirect("/view_todos")
+
+@app.route("/update_todo/<id>", methods = ['POST', 'GET'])
+def update_todo(id):
+    if request.method == "POST":
+        form = TodoForm(request.form)
+        todo_name = form.name.data
+        todo_description = form.description.data
+        completed = form.completed.data
+
+        db.todos_flask.find_one_and_update({"_id": ObjectId(id)}, {"$set": {
+            "name": todo_name,
+            "description": todo_description,
+            "completed": completed,
+            "date completed": datetime.now()
+        }})
+        flash("Task successfully updated", "success")
+        return redirect("/view_todos")
+    else:
+        form = TodoForm()
+
+        todo = db.todos_flask.find_one_or_404({"_id": ObjectId(id)})
+        print(todo)
+        form.name.data = todo.get("name", None)
+        form.description.data = todo.get("description", None)
+        form.completed.data = todo.get("completed", None)
+
     return render_template("add_todo.html", form = form)
